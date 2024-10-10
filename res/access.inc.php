f0e9c24e65bf2384857c4bb7f97246431b680cd4
Marco071278 <marcocrux12@gmail.com>
2024-10-09 22:41:12 -0600

    Add files via upload

---
+++ res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'pwpp757p';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('m2nbj3q2'),
+		'id' => 'm2nbj3q2',
+		'name' => 'Admin',
+		'password' => '31c35bs9',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'nuevousuario' => array(
+		'groups' => array('e6mc28z2'),
+		'id' => 'e6mc28z2',
+		'name' => 'Nuevousuario',
+		'password' => '2jayvy61',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('m2nbj3q2');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
