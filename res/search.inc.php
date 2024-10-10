96bab7c534425077d4a332082dd34ec554341839
Marco071278 <marcocrux12@gmail.com>
2024-10-09 22:59:39 -0600

    Add files via upload

---
+++ res/search.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.html',
+		'3' => 'lavadoras-2-tinas.html',
+		'5' => 'pagina-3.html',
+		'6' => 'lavadoras-1-tina.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	PRODUCTS SEARCH
+*****************/
+$imSettings['search']['products'] = array(
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+$imSettings['search']['dynamicobjects'] = array(
+);
+
+// End of file search.inc.php
